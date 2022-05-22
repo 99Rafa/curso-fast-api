32b1c5131b65d12d41885bf373d9f3a79d9a428d
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-21 19:56:10 -0600

    validations_path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import Body, FastAPI, Query
+from fastapi import Body, FastAPI, Path, Query
 
 from models import Person
 
@@ -22,8 +22,31 @@ def create_person(person: Person = Body(...)):
 # Validations: Query parameters
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(default=None, min_length=3, max_length=50),
-    # This makes age obligatory which is not a recommended practice
-    age: int = Query(...),
+    name: Optional[str] = Query(
+        default=None,
+        min_length=3,
+        max_length=50,
+        title="Person name",
+        description="This is the person name, it's between 1 and 50 characters",
+    ),
+    age: int = Query(
+        # This makes age obligatory which is not a recommended practice
+        ...,
+        title="Person age",
+        description="This is the person age, it's required",
+    ),
 ):
     return {name: age}
+
+
+# Validations: Path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person id",
+        description="This is the person id.",
+    )
+):
+    return {person_id: "It exists"}
