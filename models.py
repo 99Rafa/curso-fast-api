5f4266bc16cdb416b87ae125850c6c334393b135
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-21 21:03:51 -0600

    validations_models

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from enums import HairColor
 
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(..., min_length=3, max_length=50)
+    last_name: str = Field(..., min_length=3, max_length=50)
+    age: int = Field(..., gt=0)
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 
 class Location(BaseModel):
