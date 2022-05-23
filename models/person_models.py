c51a6dc895fdf555ed617147280bf786ace72557
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-22 18:34:18 -0600

    remove_duplicates

--- models/person_models.py
+++ models/person_models.py
@@ -1,4 +1,5 @@
 from asyncio.base_subprocess import BaseSubprocessTransport
+from doctest import Example
 from typing import Optional
 
 from pydantic import BaseModel, Field
@@ -6,36 +7,41 @@ from pydantic import BaseModel, Field
 from enums import HairColor
 
 
-class Person(BaseModel):
-    first_name: str = Field(..., min_length=3, max_length=50)
-    last_name: str = Field(..., min_length=3, max_length=50)
-    age: int = Field(..., gt=0)
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)
+class BasePerson(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        example="Rafael",
+    )
+    last_name: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        example="Aguirre",
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        example=22,
+    )
+    hair_color: Optional[HairColor] = Field(
+        default=None,
+        example="black",
+    )
+    is_married: Optional[bool] = Field(
+        default=None,
+        example=False,
+    )
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "first_name": "Rafael",
-                "last_name": "Aguirre",
-                "age": 22,
-                "hair_color": "black",
-                "is_married": False,
-            }
-        }
+
+class Person(BasePerson):
+    pass
 
 
-class CreatePersonRequest(Person):
-    password: str = Field(..., min_length=8)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "first_name": "Rafael",
-                "last_name": "Aguirre",
-                "age": 22,
-                "hair_color": "black",
-                "is_married": False,
-                "password": "12345678",
-            }
-        }
+class CreatePersonRequest(BasePerson):
+    password: str = Field(
+        ...,
+        min_length=8,
+        example="12345678",
+    )
